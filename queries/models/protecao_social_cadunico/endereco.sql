ad837a19384d7126502a21b71fecc6a61a7d3685
d116626 <d116626@gmail.com>
2023-12-08 18:49:33 -0300

    chore: update endereco model

--- queries/models/protecao_social_cadunico/endereco.sql
+++ queries/models/protecao_social_cadunico/endereco.sql
@@ -1,45 +1,108 @@
--- CREATE OR REPLACE TABLE `rj-smas.protecao_social_cadunico.bairro` AS (
+CREATE OR REPLACE TABLE `rj-smas.protecao_social_cadunico.endereco` AS (
 
-WITH address AS (
-    SELECT 
-        bairro_limpo_cadunico AS nome_limpo_cadunico,
-        SUM(count) as count
-    FROM `rj-smas.protecao_social_cadunico.endereco`
-    GROUP BY  1
+WITH endereco_tb AS (
+  SELECT
+    CONCAT(
+      CASE
+        WHEN tipo_logradouro IS NULL THEN ""
+        WHEN SAFE_CAST(tipo_logradouro AS INT64) IS NOT NULL THEN ""
+        ELSE TRIM(REGEXP_REPLACE(REGEXP_REPLACE(tipo_logradouro, r'\s+', ' '), r'[0-9]', ''))
+
+      END,
+      " ",
+      CASE
+        WHEN titulo_logradouro IS NULL THEN ""
+        WHEN SAFE_CAST(titulo_logradouro AS INT64) IS NOT NULL THEN ""
+        ELSE TRIM(REGEXP_REPLACE(REGEXP_REPLACE(titulo_logradouro, r'\s+', ' '), r'[0-9]', ''))
+      END,
+      " ",
+      CASE
+        WHEN logradouro IS NULL THEN ""
+        ELSE TRIM(REGEXP_REPLACE(logradouro, r'\s+', ' '))
+      END,
+      " ",
+      CASE
+        WHEN numero_logradouro IS NULL THEN "S/N"
+        ELSE CAST(numero_logradouro AS STRING)
+      END,
+      ", RIO DE JANEIRO, RJ, BRASIL"
+    ) AS address,
+    CASE
+      WHEN localidade IS NULL THEN ""
+      WHEN SAFE_CAST(localidade AS INT64) IS NOT NULL THEN ""
+      ELSE TRIM(REGEXP_REPLACE(REGEXP_REPLACE(localidade, r'\s+', ' '), r'[0-9]', ''))
+    END AS bairro_cadunico,
+    REGEXP_REPLACE(
+      REGEXP_REPLACE(
+        LOWER(NORMALIZE(CASE
+          WHEN localidade IS NULL THEN ""
+          WHEN SAFE_CAST(localidade AS INT64) IS NOT NULL THEN ""
+          ELSE TRIM(REGEXP_REPLACE(REGEXP_REPLACE(localidade, r'\s+', ' '), r'[0-9]', ''))
+        END, NFD)), r'[^a-z0-9]+', ''),
+      r'\s+', ' '
+    ) AS bairro_limpo_cadunico,
+  FROM `rj-smas.protecao_social_cadunico.identificacao_controle`
 ),
 
-address_percentage AS (
-  SELECT 
-    a.nome_limpo_cadunico,
-    count,
-    ROUND(count * 100.0 / SUM(count) OVER (), 5) AS percentage,
-  FROM address a
-  ORDER BY count DESC
+address_tb AS (
+    SELECT
+      REGEXP_REPLACE(address, r'\s+', ' ') AS address,
+      bairro_cadunico,
+      bairro_limpo_cadunico,
+      count(address) as count,
+      ROUND(COUNT(address) * 100.0 / (SELECT COUNT(*) FROM endereco_tb), 2) AS percentage,
+      ROUND(SUM(COUNT(address) * 100.0 / (SELECT COUNT(*) FROM endereco_tb)) OVER (ORDER BY COUNT(address) DESC) , 2) AS cumulative_percentage,
+  FROM endereco_tb
+  GROUP BY 1, 2, 3
+  ORDER BY 4 DESC
+),
+
+address_prep_tb AS (
+    SELECT
+        address,
+        SPLIT(address, ',') AS address_array,
+        bairro_cadunico,
+        bairro_limpo_cadunico,
+        count,
+        percentage,
+        cumulative_percentage,
+    FROM address_tb
 ),
 
 bairro AS (
   SELECT
     id_bairro,
-    nome,
+    bairro_limpo,
+    bairro,
     subprefeitura,
-    geometry_wkt,
-    REGEXP_REPLACE(REGEXP_REPLACE(LOWER(NORMALIZE(nome, NFD)), r'[^a-z0-9]+', ''), r'\s+', ' ') AS nome_limpo
-  FROM `rj-escritorio-dev.dados_mestres.bairro`
-
+  FROM `rj-smas.protecao_social_cadunico.bairro`
 )
 
-SELECT 
-    id_bairro,
-    b.nome,
-    b.nome_limpo,
-    a.nome_limpo_cadunico,
-    a.count,
-    a.percentage,
-    ROUND(SUM(percentage) OVER (ORDER BY count DESC), 2) AS cumulative_percentage,
-    subprefeitura,
-    geometry_wkt,
-FROM address_percentage a
-FULL OUTER JOIN bairro b
-  ON a.nome_limpo_cadunico = b.nome_limpo
+SELECT
+  REGEXP_REPLACE(
+    CONCAT(
+      address_array[OFFSET(0)],
+      ', ',
+      CASE
+        WHEN b.bairro IS NULL THEN ""
+        ELSE UPPER(b.bairro)
+      END,
+      ', ',
+      ARRAY_TO_STRING(ARRAY(SELECT x FROM UNNEST(address_array) x WITH OFFSET o WHERE o > 0), ', ')
+    ), r',\s*,',","
+  ) AS address,
+  b.id_bairro,
+  b.bairro AS bairro,
+  b.bairro_limpo,
+  a.bairro_cadunico,
+  a.bairro_limpo_cadunico,
+  b.subprefeitura,
+  a.count,
+  a.percentage,
+  a.cumulative_percentage,
+FROM address_prep_tb a
+LEFT JOIN bairro b
+  ON a.bairro_limpo_cadunico = b.bairro_limpo
 ORDER BY count DESC
--- )
+LIMIT 10
+)
