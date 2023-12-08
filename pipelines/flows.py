d498650ea70e0bc76aff8f2f82a1eb83feb7b52f
d116626 <d116626@gmail.com>
2023-12-08 18:57:29 -0300

    chore: change constants agent

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -2,5 +2,5 @@
 """
 Imports all flows for every project so we can register all of them.
 """
-from pipelines.exemplo import *  # noqa
 from pipelines.cadunico import *  # noqa
+from pipelines.exemplo import *  # noqa
