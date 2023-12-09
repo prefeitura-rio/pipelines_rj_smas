4ed1cfd0856cc2358e9f11ceacf69a0d89ce715e
d116626 <d116626@gmail.com>
2023-12-09 04:45:57 -0300

    chore: pump prefeitura-rio version

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -4,3 +4,4 @@ Imports all flows for every project so we can register all of them
 """
 from pipelines.cadunico import *  # noqa
 from pipelines.exemplo import *  # noqa
+from pipelines.templates import *  # noqa
