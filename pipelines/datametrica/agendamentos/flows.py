9a9c3b512f5a835bd76a845f28f6d32d06843777
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-06-30 12:15:40 -0300

    feat: boilerplate da pipeline de ingestao da datametrica

---
+++ pipelines/datametrica/agendamentos/flows.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from prefect import Parameter
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefeitura_rio.pipelines_utils.custom import Flow
+from prefeitura_rio.pipelines_utils.state_handlers import (
+    handler_initialize_sentry,
+    handler_inject_bd_credentials,
+)
+from prefeitura_rio.pipelines_utils.tasks import create_table_and_upload_to_gcs
+
+from pipelines.constants import constants
+from pipelines.datametrica.agendamentos.tasks import (
+    get_datametrica_credentials,
+    fetch_agendamentos_from_api,
+    transform_agendamentos_data,
+    convert_agendamentos_to_dataframe
+)
+from pipelines.utils.tasks import create_date_partitions
+
+with Flow(
+    name="rj-smas: Datametrica - Extração de agendamentos",
+    state_handlers=[
+        handler_initialize_sentry,
+        handler_inject_bd_credentials,
+    ],
+    parallelism=10,
+    skip_if_running=False,
+) as datametrica__agendamentos__flow:
+    
+    #########################
+    #  Define parameters    #
+    #########################
+    
+    dataset_id = Parameter("dataset_id", default="brutos_data_metrica_staging", required=False)
+    table_id = Parameter("table_id", default="agendamentos_cadunicos", required=False)
+    dump_mode = Parameter("dump_mode", default="append", required=False)
+    date_param = Parameter("date", default=None, required=False)
+    
+    #########################
+    #  Start flow           #
+    #########################
+    
+    # Get API credentials
+    credentials = get_datametrica_credentials()
+    
+    # Fetch data from API
+    raw_data = fetch_agendamentos_from_api(credentials=credentials, date=date_param)
+    
+    # Transform data to typed objects
+    processed_data = transform_agendamentos_data(raw_data)
+    
+    # Convert to DataFrame
+    df = convert_agendamentos_to_dataframe(processed_data)
+    
+    # Create date partitions
+    partitions_path = create_date_partitions(
+        dataframe=df,
+        partition_column="data_hora",
+        file_format="csv",
+        root_folder="./data_agendamentos/",
+    )
+    
+    # Upload to GCS and BigQuery
+    create_table = create_table_and_upload_to_gcs(
+        data_path=partitions_path,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode=dump_mode,
+        biglake_table=False,
+    )
+
+# Storage and run configs
+datametrica__agendamentos__flow.state_handlers = [handler_inject_bd_credentials]
+datametrica__agendamentos__flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+datametrica__agendamentos__flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+datametrica__agendamentos__flow.executor = LocalDaskExecutor(num_workers=1)
---
+++ pipelines/datametrica/agendamentos/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+from typing import Optional
+
+
+def build_agendamentos_url(base_url: str, date: Optional[str] = None) -> str:
+    """
+    Constrói a URL para buscar agendamentos da API da Datametrica.
+    
+    Args:
+        base_url: URL base da API (vem do Infisical)
+        date: Data no formato YYYY-MM-DD. Se None, usa o dia seguinte.
+        
+    Returns:
+        URL completa para a API
+    """
+    if date is None:
+        # Data de amanhã no formato YYYY-MM-DD
+        date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+    
+    # Remove trailing slash se existir
+    base_url = base_url.rstrip('/')
+    return f"{base_url}/api/agendamentos/{date}"
