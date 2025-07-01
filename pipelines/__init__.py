4f911e7877b51cee11390b8afbc7280d0e39849b
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-07-01 13:14:44 -0300

    init

---
+++ pipelines/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Pipelines package for Rio de Janeiro SMAS
+"""
+
+__version__ = "1.0.0"
+
+# Import submódulos para torná-los disponíveis via pipelines.xxx
+from pipelines import api_datametrica  # noqa
+from pipelines import utils  # noqa
