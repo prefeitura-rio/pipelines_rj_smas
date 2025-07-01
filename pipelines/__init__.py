edd57acdf248e61ab4ad967c566ebf66fc02819e
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-07-01 13:20:10 -0300

    fix utf-8 error

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -5,6 +5,5 @@ Pipelines package for Rio de Janeiro SMAS
 
 __version__ = "1.0.0"
 
-# Import submódulos para torná-los disponíveis via pipelines.xxx
 from pipelines import api_datametrica  # noqa
 from pipelines import utils  # noqa
