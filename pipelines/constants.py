ae25633e63e4efbc3389cd31eba0b0d678760a35
Joao Pedro Oliveira <joaopsantos@pm.me>
2025-07-02 11:48:16 -0300

    adds slash to path

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -22,6 +22,6 @@ class constants(Enum):
     # Other constants
     ######################################
     # EXAMPLE_CONSTANT = "example_constant"
-    DATAMETRICA_PATH = "api_data_metrica"
+    DATAMETRICA_PATH = "/api_data_metrica"
     DATAMETRICA_URL = "URL"
     DATAMETRICA_TOKEN = "TOKEN"
